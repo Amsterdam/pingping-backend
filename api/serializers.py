aa4c2b9d281a0f835e7d068c59c6e7837eedda7d
unknown <luismiguel.mopa@gmail.com>
2019-09-17 13:38:41 -0500

    show multiple like json

--- api/serializers.py
+++ api/serializers.py
@@ -265,7 +265,11 @@ class QuestionSerializer(serializers.ModelSerializer):
     previousQuestion = serializers.SerializerMethodField()
     currentQuestion = serializers.ReadOnlyField(source='id')
     numberOfQuestions = serializers.SerializerMethodField()
+    multiple = serializers.SerializerMethodField()
     order = serializers.ReadOnlyField(source='get_order')
+
+    def get_multiple(self, obj):
+        return obj.multiple
 
     def get_answer(self, obj):
         return {
