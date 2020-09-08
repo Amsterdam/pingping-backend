3dd8b616eea8aae4ba44e7a17a5158e880d374ab
Mani Gudvardarson <m.gudvardarson@amsterdam.nl>
2020-09-08 16:08:04 +0200

    Fixed tests & server handler

---
+++ src/App.ts
@@ -0,0 +1,49 @@
+import _ from 'lodash';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import boot from './boot';
+import { AppModule } from 'modules/app';
+import { getContext as context } from 'lib/Context';
+
+import { ApolloServerPlugin } from 'apollo-server-plugin-base';
+
+import { GraphQLRequestContext } from 'apollo-server-types';
+import { GraphQLRequestListener } from 'apollo-server-plugin-base/src/index';
+
+export const LogPlugin: ApolloServerPlugin = {
+  requestDidStart<TContext>(_: GraphQLRequestContext<TContext>): GraphQLRequestListener<TContext> {
+    return {
+      didEncounterErrors(context) {
+        console.log('error:', context.errors);
+      },
+    };
+  },
+};
+
+export class App {
+  public app: any;
+
+  constructor() {
+    this.app = express();
+    boot.start();
+
+    const { schema } = AppModule.forRoot({ layouts: [] });
+    const server = new ApolloServer({
+      schema,
+      context,
+      plugins: [LogPlugin],
+    });
+
+    this.app.use(express.static('public'));
+    this.registerStatusHealthRoute();
+    server.applyMiddleware({ app: this.app, path: '/api' });
+  }
+
+  registerStatusHealthRoute() {
+    this.app.get('/status/health', (req: any, res: any) => {
+      res.send('OK');
+    });
+  }
+}
+
+export default new App().app;
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,18 @@
-import createServer from './server';
-import express from 'express';
-import boot from './boot';
-
-boot.start();
-const server = createServer();
+import 'reflect-metadata';
+import App from './App';
 
 const corsOptions: object = {
   credentials: true,
   origin: process.env.APP_URL,
 };
 
-server.use(express.static('public'));
-
-server.get('/status/health', (req, res) => {
-  res.send('OK');
-});
-
-server.listen(
+App.listen(
   {
-    port: process.env.PORT || 4000,
     cors: corsOptions,
+    port: process.env.PORT || 4000,
     playground: process.env.NODE_ENV === 'production' ? false : '/api',
   },
   () => {
     console.log(`Server started: ${process.env.PORT || 4000}`);
-    return server;
   }
 );
-
-export default server;
