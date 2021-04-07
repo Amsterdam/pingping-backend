5fad83b2ce95c87e1baa8df3a7ae857b2a387fe7
Mani Gudvardarson <m.gudvardarson@amsterdam.nl>
2021-04-07 09:14:43 +0200

    env tag for header fixed

--- src/modules/admin/resolvers/Query.ts
+++ src/modules/admin/resolvers/Query.ts
@@ -53,7 +53,7 @@ const BASE_QUERY: any = {
 
 export const Query: QueryResolvers = {
   getEnv() {
-    return `${process.env.ENVIRONMENT || 'unknown'}:${process.env.NODE_ENV}`;
+    return process.env.ENVIRONMENT || 'unknown';
   },
   async getDraftNotification(root: any, args: QueryGetDraftNotificationArgs, context: ContextType): Promise<any> {
     switch (args.type) {
